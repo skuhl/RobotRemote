83f7e1e8a1b4234e5fe3044502bb07e86c93e3c5
Brandon Johnson <zebrahome10@gmail.com>
2018-05-22 16:47:19 -0400

    cast from bit to boolean.

--- node/user_auth.js
+++ node/user_auth.js
@@ -8,7 +8,26 @@ module.exports = {
             host: host,
             user: username,
             password: password,
-            database: database
+            database: database,
+            /*This code snippet found from  https://www.bennadel.com/blog/3188-casting-bit-fields-to-booleans-using-the-node-js-mysql-driver.htm*/
+            typeCast: function castField( field, useDefaultTypeCasting ) {
+
+                // We only want to cast bit fields that have a single-bit in them. If the field
+                // has more than one bit, then we cannot assume it is supposed to be a Boolean.
+                if ( ( field.type === "BIT" ) && ( field.length === 1 ) ) {
+        
+                    var bytes = field.buffer();
+
+                    // A Buffer in Node represents a collection of 8-bit unsigned integers.
+                    // Therefore, our single "bit field" comes back as the bits '0000 0001',
+                    // which is equivalent to the number 1.
+                    return( bytes[ 0 ] === 1 );
+        
+                }
+        
+                return( useDefaultTypeCasting() );
+        
+            }
         });
     },
     /* 
@@ -36,7 +55,9 @@ module.exports = {
                     connection.end(()=>{});
                     return;
                 }
-                
+
+                console.log(res);
+
                 if(res.length > 1){
                     reject({
                         reason: 'More than 1 user with given email!',
