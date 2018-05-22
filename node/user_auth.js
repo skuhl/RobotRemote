868444fc5513990b9f4819471c7458c5f848a8b8
Brandon Johnson <zebrahome10@gmail.com>
2018-05-22 16:34:05 -0400

    Mispelled console.

--- node/user_auth.js
+++ node/user_auth.js
@@ -22,7 +22,7 @@ module.exports = {
         return new Promise ((resolve, reject) => {
             connection.connect((err)=>{
                 if(err){
-                    consle.log(err);
+                    console.log(err);
                     reject({
                         reason: 'Unable to connect to database',
                         client_reason: 'Unable to connect to database'
