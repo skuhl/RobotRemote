b45cd81aacc24f988729dc78bb4bab817916e983
Brandon Johnson <zebrahome10@gmail.com>
2018-05-23 11:00:12 -0400

    Database procedure, fix style of code snippet.

--- helperscripts/db_setup.sql
+++ helperscripts/db_setup.sql
@@ -11,8 +11,8 @@ CREATE TABLE IF NOT EXISTS users (id INT UNSIGNED AUTO_INCREMENT KEY NOT NULL,
                                   loginreq_id INT UNSIGNED NULL);
 /*Create user login request table*/
 CREATE TABLE IF NOT EXISTS loginrequests (id INT UNSIGNED AUTO_INCREMENT KEY NOT NULL,
-                                          email_token CHAR(32) NOT NULL UNIQUE,
-                                          email_validated BIT(1) NOT NULL,
+                                          email_token CHAR(32) NOT NULL,
+                                          email_validated BIT(1) NOT NULL DEFAULT 0,
                                           comment TEXT NOT NULL);
 /*create timeslot request table*/
 CREATE TABLE IF NOT EXISTS timeslots (id INT UNSIGNED AUTO_INCREMENT KEY NOT NULL,
@@ -27,3 +27,15 @@ ALTER TABLE timeslots ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES
 /*Setup indices*/
 ALTER TABLE users ADD INDEX email (email); /*This one should fail, unique implies an index*/
 ALTER TABLE timeslots ADD INDEX user_id (user_id);
+
+/*Procedures*/
+DELIMITER //
+CREATE PROCEDURE user_request
+(IN email_in CHAR(191), IN passwordhash CHAR(64), IN passwordsalt CHAR(16), IN email_token_in CHAR(32), IN user_comment)
+BEGIN
+    /*Make a new request for a login*/
+    INSERT INTO loginrequests (email_token, comment, email_validated) VALUES (email_token_in, user_comment, 0);
+    /*Make a new unapproved user*/
+    INSERT INTO users (email, passhash, passsalt, loginreq_id, approved, admin) 
+        VALUES (email_in, passwordhash, passwordsalt, LAST_INSERT_ID(), 0, 0);
+END//
