ffdb1a3cdd54f170a0db2242ab67b46f52f18b29
Brandon Johnson <zebrahome10@gmail.com>
2018-05-24 13:58:42 -0400

    Transition messages in.

--- node/www/js/login.js
+++ node/www/js/login.js
@@ -1,7 +1,10 @@
 var CheckError = function(){
     let msg_component = document.getElementById("errmsg");
-    if(!msg_component) return;
     
+    if(!msg_component) return;
     let msg = msg_component.value;
+
     document.getElementById("msg_text").innerHTML = msg;
+    document.getElementById("msg_container").style.visibility = "visible";
+    document.getElementById("msg_container").style.opacity = 1;
 }
