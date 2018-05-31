bc462b0b73f6a56d25b57d7e67a67b499a37cc34
Mads-Claire <mhoward@mtu.edu>
2018-05-31 16:42:02 -0400

    should be all the bugs outa here Just gotta make it submit properly again

--- node/www/js/validate.js
+++ node/www/js/validate.js
@@ -4,28 +4,6 @@ var passConfirm = form.elements.REpassword;
 var email = form.elements.username;
 var reason = form.elements.reason;
 
-passConfirm.addEventListener("keyup", function(event){
-	//Check that the passwords match
-	if(password.value != passConfirm.value)
-	{
-		console.log('SetCustomValidity');
-		event.target.setCustomValidity("Passwords don't match!");
-	}else
-	{
-		event.target.setCustomValidity("");
-	}
-});
-
-reason.addEventListener("keyup", function(event){
-	if(reason === null || reason === "")
-	{
-		reason.setCustomValidity("Please provide a reason for your request!");
-	}else
-	{
-		reason.setCustomValidity("");
-	}
-});
-
 function validate()
 {
 	//Check that the passwords match
