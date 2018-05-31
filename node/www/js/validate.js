c19a9fd9f8c09c43e920d36f46866b619ea5823b
Mads-Claire <mhoward@mtu.edu>
2018-05-31 16:00:02 -0400

    The current function is interesting...

--- node/www/js/validate.js
+++ node/www/js/validate.js
@@ -4,54 +4,11 @@ var passConfirm = form.elements.REpassword.value;
 var email = form.elements.username.value;
 var reason = form.elements.reason.value;
 
-email.addEventListener("keyup", function(event){
-	if(!email.validity.valid){
-		email.setCustomValidity("Email format nonstandard!");
-	}
-	else{
-		email.setCustomValidity("");
-	}
-
-},false);
-
-passConfirm.addEventListener("keyup", function(event){
-	//Check that the passwords match
-	if(password.value != passConfirm.value)
-	{
-		console.log('SetCustomValidity');
-		passConfirm.setCustomValidity("Passwords don't match!");
-	}else
-	{
-		passConfirm.setCustomValidity("");
-	}
-}, false);
-
-reason.addEventListener("keyup", function(event){
-	if(reason === null || reason === "")
-	{
-		reason.setCustomValidity("Please provide a reason for your request!");
-	}else
-	{
-		reason.setCustomValidity("");
-	}
-},false);
-
 function validate()
 {
 	console.log('validate called');
-	//Regular expression checks for email in format " "@" "." "
-	var n = email.search(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/);
-	//might need to be less than one?
-	if(n < 0)
-	{
-		email.setCustomValidity("Email format nonstandard!");
-	}else
-	{
-		email.setCustomValidity("");
-	}
-	
 	//Check that the passwords match
-	if(password.value != passConfirm.value)
+	if(password.value !== passConfirm.value)
 	{
 		console.log('SetCustomValidity');
 		passConfirm.setCustomValidity("Passwords don't match!");
@@ -60,7 +17,7 @@ function validate()
 		passConfirm.setCustomValidity("");
 	}
 	
-	if(reason === null || reason === "")
+	if(reason == null || reason == "")
 	{
 		reason.setCustomValidity("Please provide a reason for your request!");
 	}else
