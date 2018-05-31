9e366d3b7e4fe782d9aaa3b2b3f616d2c179f09f
Mads-Claire <mhoward@mtu.edu>
2018-05-31 14:10:16 -0400

    There's a whole big mess happening

--- node/www/js/validate.js
+++ node/www/js/validate.js
@@ -4,6 +4,38 @@ var passConfirm = form.elements.REpassword.value;
 var email = form.elements.username.value;
 var reason = form.elements.reason.value;
 
+email.addEventListener("input", function(event){
+	if(!email.validity.valid){
+		email.setCustomValidity("Email format nonstandard!");
+	}
+	else{
+		email.setCustomValidity("");
+	}
+
+},false);
+
+passConfirm.addEventListener("input", function(event){
+	//Check that the passwords match
+	if(password.value != passConfirm.value)
+	{
+		console.log('SetCustomValidity');
+		passConfirm.setCustomValidity("Passwords don't match!");
+	}else
+	{
+		passConfirm.setCustomValidity("");
+	}
+}, false);
+
+reason.addEventListener("input", function(event){
+	if(reason === null || reason === "")
+	{
+		reason.setCustomValidity("Please provide a reason for your request!");
+	}else
+	{
+		reason.setCustomValidity("");
+	}
+},false);
+
 function validate()
 {
 	console.log('validate called');
@@ -37,5 +69,7 @@ function validate()
 	}
 }
 
+email.onchange = validate;
 password.onchange = validate;
 passConfirm.onkeyup = validate;
+reason.onchange= validate;
