a77c0649825a53e8db8f4258dd4d7ba35b3509b5
Brandon Johnson <zebrahome10@gmail.com>
2018-06-01 11:19:29 -0400

    Change messages to be a little more user friendly (maybe).

--- node/www/js/submit_request.js
+++ node/www/js/submit_request.js
@@ -4,21 +4,22 @@ var SubmitRequest = function(){
 	var user = form.elements.username.value;
 	var pass = form.elements.password.value;
 	var reason = form.elements.reason.value;
+
 	if(!user || !pass || !reason){ //in the scenario that one of these are missing
-		alert('Missing username, password, or reason for request.</br> These items are required.');
+		alert('Missing username, password, or reason for request.<br/> These items are required.');
 		return;
 	}
 	
 	xms.onreadystatechange = function(){
 		if(this.readyState === 4 && this.status === 200){
-			alert('Successfully added user to DB, awaiting approval.');
+			alert('Successfully signed up, verify your email and wait for approval!');
 			//Hey did that just work? maybe
 			console.log('Registered user');
 			//redirect to home or login???
 			location.replace('http://'+ window.location.host + '/Home.html');
 		}else if(this.readyState === 4){
 			//something goes wrong
-			alert('Error adding user to DB, ' + this.responseText);
+			alert('Error, ' + this.responseText);
 		}
 	}
 	xms.open("POST", 'http://' + window.location.host+ '/Request.html', true);
