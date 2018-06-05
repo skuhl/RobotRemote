a1aeb7e81b9c267836c840382d8b7a53b28d6dd5
Mads-Claire <mhoward@mtu.edu>
2018-06-05 16:40:16 -0400

    Learining RegEx may not quite work yet

--- node/www/js/validate.js
+++ node/www/js/validate.js
@@ -1,19 +1,34 @@
 var form = document.forms.signup;
-var password = form.elements.password;
-var passConfirm = form.elements.REpassword;
+var password = form.elements.password.value;
+var passConfirm = form.elements.REpassword.value;
 var reason = form.elements.reason;
 
 function validate()
 {
 	//Check that the passwords match
-	if(password.value != passConfirm.value)
+	if(password != passConfirm)
 	{
-		console.log('SetCustomValidity');
 		passConfirm.setCustomValidity("Passwords don't match!");
-	}else
+	}else if(password.length < 8 || password.length > 22){
+		password.setCustomValidity("Password should be 8-22 characters");
+	}else if(!password.includes(/([A-Z])/g)){
+		passConfirm.setCustomValidity("Missing a capital letter");
+	}
+	else if(!password.includes(/([a-z])/g)){
+		passConfirm.setCustomValidity("Missing a lower case letter");
+	}
+	else if(!password.includes(/([0-9])/g)){
+		passConfirm.setCustomValidity("Missing a number");
+	}
+	else if(!password.includes(/([0-9])/g)){//some range of special characters
+		passConfirm.setCustomValidity("Missing a number");
+	}
+	else
 	{
 		passConfirm.setCustomValidity("");
 	}
+	
+	if(password)
 	
 	if(reason === null || reason === "")
 	{
