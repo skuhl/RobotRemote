e5c5b41068ff23c4645a58a246179c0bbacc03b4
Mads-Claire <mhoward@mtu.edu>
2018-06-07 10:44:01 -0400

    we'll try this!

--- node/www/js/admin.js
+++ node/www/js/admin.js
@@ -7,7 +7,7 @@ function GenerateTimeslotTable(timeslots){
 
     var table = document.createElement('table');
     table.classList.add("admin_timeslot_req_table");
-	 table[0].rows.classList.add("admin_timeslot_rows");
+	 table.rows.className =("admin_timeslot_rows");
 	 
     table.appendChild(CreateTableRow([
         "Email",
