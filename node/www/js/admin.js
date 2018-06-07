dc27aa5af9380cc5525f6a70744a7ca051e4cf48
Mads-Claire <mhoward@mtu.edu>
2018-06-07 10:34:55 -0400

    exparimenting Still trying to do the same thing this change was suggusted online

--- node/www/js/admin.js
+++ node/www/js/admin.js
@@ -7,7 +7,7 @@ function GenerateTimeslotTable(timeslots){
 
     var table = document.createElement('table');
     table.classList.add("admin_timeslot_req_table");
-	 table.rows.classList.add("admin_timeslot_rows");
+	 table[0].rows.classList.add("admin_timeslot_rows");
 	 
     table.appendChild(CreateTableRow([
         "Email",
