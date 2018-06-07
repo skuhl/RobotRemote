fef26c2fcf5b0a8d0fa8a1012fb90f98467ba0ea
Mads-Claire <mhoward@mtu.edu>
2018-06-07 15:09:09 -0400

    Setting display

--- node/www/js/validate.js
+++ node/www/js/validate.js
@@ -17,24 +17,31 @@ function validate()
 		//Check that the passwords match
 		if(password.value != passConfirm.value){
 			document.getElementById("repass_error").innerHTML = "Passwords don't match";
+			document.getElementById("repass_error").style.display = "inline";
 		}
 		else if(password.value.length != 0 && (password.value.length < 8 || password.value.length > 22)){
 			document.getElementById("pass_error").innerHTML = "Password should be 8-22 characters";
+			document.getElementById("pass_error").style.display = "inline";
 		}
 		else if(password.value.length != 0 && !cap.test(password.value)){
 			document.getElementById("pass_error").innerHTML = "Missing a capital letter";
+			document.getElementById("pass_error").style.display = "inline";
 		}
 		else if(password.value.length != 0 && !low.test(password.value)){
 			document.getElementById("pass_error").innerHTML = "Missing a lower case letter";
+			document.getElementById("pass_error").style.display = "inline";
 		}
 		else if(password.value.length != 0 && !num.test(password.value)){
 			document.getElementById("pass_error").innerHTML = "Missing a number";
+			document.getElementById("pass_error").style.display = "inline";
 		}
 		else if(password.value.length != 0 && !spc.test(password.value)){
 			document.getElementById("pass_error").innerHTML = "Missing a special character";
+			document.getElementById("pass_error").style.display = "inline";
 		}
 		else{
 			document.getElementById("pass_error").innerHTML = "";
+			document.getElementById("pass_error").style.display = "none";
 		}
 	},500);
 }
