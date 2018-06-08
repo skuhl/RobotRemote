7f667ba5498f80b7fe18d5c72910a2d232f29820
Mads-Claire <mhoward@mtu.edu>
2018-06-08 10:55:05 -0400

    Added the delay back in so the function
    Trying the wait again so that the function validation waits until the person stops typing

--- node/www/js/validate.js
+++ node/www/js/validate.js
@@ -15,45 +15,49 @@ function validate()
 	var num = /([0-9])/;
 	var spc = /([ -/]|[:-@]|[[-`]|[{-~])/; //covers any standard special character
 	
-	//Check that the passwords match
-	if(password.value != passConfirm.value && password.value.length != 0 && passConfirm.value.length != 0){
-		document.getElementById("repass_error").innerHTML = "Passwords don't match";
-		document.getElementById("repass_error").style.display = "inline";
-	}
-	else{
-		document.getElementById("repass_error").innerHTML = "";
-		document.getElementById("repass_error").style.display = "none";
-		pass[0] = 1;
-	}
+	clearTimeout(timeout);
 	
-	if(password.value.length != 0 && (password.value.length < 8 || password.value.length > 22)){
-		console.log(password.value.length);
-		document.getElementById("pass_error").innerHTML = "Password should be 8-22 characters";
-		document.getElementById("pass_error").style.display = "inline";
-	}
-	else if(password.value.length != 0 && !cap.test(password.value)){
-		document.getElementById("pass_error").innerHTML = "Missing a capital letter";
-		document.getElementById("pass_error").style.display = "inline";
-	}
-	else if(password.value.length != 0 && !low.test(password.value)){
-		document.getElementById("pass_error").innerHTML = "Missing a lower case letter";
-		document.getElementById("pass_error").style.display = "inline";
-	}
-	else if(password.value.length != 0 && !num.test(password.value)){
-		document.getElementById("pass_error").innerHTML = "Missing a number";
-		document.getElementById("pass_error").style.display = "inline";
-	}
-	else if(password.value.length != 0 && !spc.test(password.value)){
-		document.getElementById("pass_error").innerHTML = "Missing a special character";
-		document.getElementById("pass_error").style.display = "inline";
-	}
-	else{
-		document.getElementById("pass_error").innerHTML = "";
-		document.getElementById("pass_error").style.display = "none";
-		pass[1] = 1;
-	}
+	timeout = setTimeout(function () {
 	
-	if(pass[0] == 1 && pass[1] ==1){
-		document.getElementById("sub_btn").disabled = false;
-	}
+		//Check that the passwords match
+		if(password.value != passConfirm.value && password.value.length != 0 && passConfirm.value.length != 0){
+			document.getElementById("repass_error").innerHTML = "Passwords don't match";
+			document.getElementById("repass_error").style.display = "inline";
+		}
+		else{
+			document.getElementById("repass_error").innerHTML = "";
+			document.getElementById("repass_error").style.display = "none";
+			pass[0] = 1;
+		}
+		
+		if(password.value.length != 0 && (password.value.length < 8 || password.value.length > 22)){
+			document.getElementById("pass_error").innerHTML = "Password should be 8-22 characters";
+			document.getElementById("pass_error").style.display = "inline";
+		}
+		else if(password.value.length != 0 && !cap.test(password.value)){
+			document.getElementById("pass_error").innerHTML = "Missing a capital letter";
+			document.getElementById("pass_error").style.display = "inline";
+		}
+		else if(password.value.length != 0 && !low.test(password.value)){
+			document.getElementById("pass_error").innerHTML = "Missing a lower case letter";
+			document.getElementById("pass_error").style.display = "inline";
+		}
+		else if(password.value.length != 0 && !num.test(password.value)){
+			document.getElementById("pass_error").innerHTML = "Missing a number";
+			document.getElementById("pass_error").style.display = "inline";
+		}
+		else if(password.value.length != 0 && !spc.test(password.value)){
+			document.getElementById("pass_error").innerHTML = "Missing a special character";
+			document.getElementById("pass_error").style.display = "inline";
+		}
+		else{
+			document.getElementById("pass_error").innerHTML = "";
+			document.getElementById("pass_error").style.display = "none";
+			pass[1] = 1;
+		}
+		
+		if(pass[0] == 1 && pass[1] ==1){
+			document.getElementById("sub_btn").disabled = false;
+		}
+	}, 500);
 }
