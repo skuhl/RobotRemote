ef490878b65f66d1050b38a7135c16a79ea76a0c
Brandon Johnson <zebrahome10@gmail.com>
2018-06-21 17:39:33 -0400

    Start test.js

---
+++ test/test.js
@@ -0,0 +1,43 @@
+const assert = require('assert');
+
+//Tests webserver endpoints
+describe('WebserverAPI', function(){
+
+});
+
+//Tests camera endpoints
+describe('CameraAPI', function(){
+
+});
+
+//Tests armserver endpoints
+describe('ArmServerAPI', function(){
+
+});
+
+//Tests webserver rendering of pages
+describe('Webserver Rendering', function(){
+
+});
+
+//Test communication between webserver and actuator
+//Using the code in actuator_comm.js
+describe('Webserver Actuator Communication', function(){
+
+});
+
+//Test communication between webserver and camera server
+//Using the code in webcam_comm.js
+describe('Webserver Camera Communication', function(){
+
+});
+
+//Test Camera Websockets
+describe('Camera Websockets', function(){
+
+});
+
+//Test Arm Websockets
+describe('Arm Websockets', function(){
+    
+})
