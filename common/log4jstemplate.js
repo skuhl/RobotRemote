10f5df9540d8eb27d9645d0e514aa891d07bc5e1
Brandon Johnson <zebrahome10@gmail.com>
2018-07-05 14:17:19 -0400

    Less naive stack navigation; Should always get a non-log4js file.

--- common/log4jstemplate.js
+++ common/log4jstemplate.js
@@ -6,20 +6,41 @@ function getStack(){
         return stack;
     }
     let err = new Error();
-    Error.stackTraceLimit = 20;
+    Error.stackTraceLimit = 25;
     Error.captureStackTrace(err);
     let stack = err.stack;
     Error.prepareStackTrace = orig;
     return stack;
 }
+
+function getNonLog4jsStack(stack){
+    for(let i = 2; i < stack.length; i++){
+        if(stack[i] === null) return null;
+        if(stack[i].getFileName() === null) continue;
+        if(stack[i].getFileName().indexOf('/node_modules/log4js/') === -1) return stack[i];
+    }
+    return null;
+}
+
 //Functions for getting tokens. This might need some tweaking (12 stack frames deep might not be consistent)
 function getLogFileName(logEvent){
-    let file_path = getStack()[11].getFileName();
-    return file_path.slice(base_dir_name.length);
+    let stack_call = getNonLog4jsStack(getStack());
+    if(stack_call === null) return 'unknown';
+    let file_name = stack_call.getFileName();
+    if(file_name == null) return 'unknown';
+    if(file_name.startsWith('/')){
+        return file_name.slice(base_dir_name.length);
+    }else{
+        return file_name;
+    }
 }
 
 function getLogLineNumber(logEvent){
-    return getStack()[11].getLineNumber();
+    let stack_call = getNonLog4jsStack(getStack());
+    if(stack_call === null) return 'unknown';
+    let line = stack_call.getLineNumber();
+    if(line == null) return 'unknown'; 
+    return line;
 }
 
 module.exports = {
